1870cff11c4b8e0b0f3c996dc6b7b1f103f81631
leo guerard <leo.david.guerard@gmail.com>
2023-09-19 18:22:53 +0200

    add the map

--- src/views/ImageView.js
+++ src/views/ImageView.js
@@ -1,29 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "../utils/api";
 
 function Picture(props) {
-    console.log(props)
-    return (
+
+    console.log(props);
+    
+    return (<div>
         <img src={props.src} alt="image"/>
+        {props.id}</div> 
     )
 }
 
-function ImageView(props) {
+function ImageView() {
+
+    const [listImage, setListImage] = useState([]);
 
     useEffect(() => {
         api(null).get('').then((res) => {
-            console.log(res.data);
+            setListImage(res.data.hits);
         }
         ).catch((err) => {
             console.log(err);
         });
     }, []);
 
+
+
     return (
         <div>
-            <Picture />
+            <ul>
+                {listImage.map((item) => ( 
+                    <li>1{item.id}</li>
+                ))}
+            </ul>
         </div>
     );
 }
 
+
 export default ImageView;
+
